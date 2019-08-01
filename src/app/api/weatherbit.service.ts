d30df43da432964c88a2e592fd0128f86e848c6b
Jesus Alberto Ochoa Portillo <switch8969@gmail.com>
2019-08-01 00:02:22 -0700

    -- Changed Background

--- src/app/api/weatherbit.service.ts
+++ src/app/api/weatherbit.service.ts
@@ -11,35 +11,36 @@ import { Component, OnInit } from '@angular/core';
   providedIn: 'root'
 })
 export class WeatherbitService {
-  private urlService = "https://api.weatherbit.io/v2.0/forecast/daily?lat=";
+  private urlService = 'https://api.weatherbit.io/v2.0/forecast/daily?lat=';
 
   constructor(private http: HttpClient)  { }
   getWeather(cname: string, utemp: string): Observable<wbitdata> {
-    var vLong: any;
-    var vLat: any;
-    switch(cname){
+    let vLong: any;
+    let vLat: any;
+    switch (cname) {
       case 'CdObregon':
         vLong = -109.9333;
         vLat = 27.4833;
-      break;
+        break;
       case 'Navojoa':
         vLong = -109.44391;
         vLat = 27.07015;
-      break;
+        break;
       case 'Hermosillo':
         vLong = -110.97732;
         vLat = 29.1026;
-      break;
+        break;
       case 'Nogales':
         vLong = -110.94217;
         vLat = 31.30862;
-      break;
+        break;
       default:
         vLong = -109.9333;
         vLat = 27.4833;
-      break;
+        break;
     }
-    return this.http.get<wbitdata>(this.urlService + vLat + "101&lon="+vLong+"&days=15&units=" + utemp + "&key=29d273d0388741e2a78d622f60a4982a");
+    // tslint:disable-next-line: max-line-length
+    return this.http.get<wbitdata>(this.urlService + vLat + '101&lon=' + vLong + '&days=15&units=' + utemp + '&key=29d273d0388741e2a78d622f60a4982a');
 
   }
 
